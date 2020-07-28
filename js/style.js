2d634894bbdb1bb3ab452b65dc00472c8fba5aa8
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-07-28 23:23:56 +0600

    All fanctionality added successfully

--- js/style.js
+++ js/style.js
@@ -19,7 +19,7 @@ document.getElementById("generate-pin").addEventListener("click", function () {
   document.getElementById("disply-pin").value = randomValue;
   document.getElementById("notify-match").style.display = "none";
   document.getElementById("notify-dont-match").style.display = "none";
-})
+});
 
 //add event listener to all number button 
 const len = document.querySelectorAll(".buton-control").length;
@@ -28,17 +28,17 @@ for (var i = 0; i < len; i++)
     const number = this.innerHTML;
     const previousValue = document.getElementById("input-pin").value;
     document.getElementById("input-pin").value = previousValue + number;
-  })
+  });
 
 //set clear button
 document.getElementById("clear").addEventListener("click", function () {
   const innerValue = document.getElementById("input-pin").value;
   document.getElementById("input-pin").value = '';
-})
+});
 
 // Pin Matching
 document.getElementById("submit").addEventListener("click", function () {
-
+  var submitChance = parseInt (document.getElementById("submit-chance").innerHTML); // chance of input password
   const randNumber = document.getElementById("disply-pin").value;
   const inputedNumber = document.getElementById("input-pin").value;
 
@@ -47,13 +47,29 @@ document.getElementById("submit").addEventListener("click", function () {
   }
   else if (randNumber == inputedNumber) {
     document.getElementById("notify-match").style.display = "block";
+      document.getElementById("disply-pin").value = '';
+  document.getElementById("input-pin").value = '';
   }
   else {
     document.getElementById("notify-dont-match").style.display = "block";
+    submitChance--; // reduce 
+    document.getElementById("submit-chance").innerHTML = submitChance;
+    if(submitChance == 0){ // disabled all feature
+      var submitButton = document.getElementById("submit");
+      var pinGenaretorButton = document.getElementById("generate-pin");
+      pinGenaretorButton.disabled = true; 
+      pinGenaretorButton.style.background = "red";
+      pinGenaretorButton.style.transition = ".5s";
+
+      submitButton.disabled = true;
+      submitButton.style.background = "red";
+      submitButton.style.transition = ".5s";
+      document.getElementById("disply-pin").value = '';
+      document.getElementById("notify-dont-match").style.display = "none";
+    }
   }
-  document.getElementById("disply-pin").value = '';
-  document.getElementById("input-pin").value = '';
-})
+  document.getElementById("input-pin").value = ''; //clear input field
+});
 
 // delete one number per click
 document.getElementById("backspace").addEventListener("click", function () {
@@ -62,4 +78,7 @@ document.getElementById("backspace").addEventListener("click", function () {
     presentValue = presentValue.slice(0, -1);
     document.getElementById("input-pin").value = presentValue;
   }
-})
+});
+
+
+
