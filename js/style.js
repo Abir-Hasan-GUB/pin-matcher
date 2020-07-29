4d44ac24ed8e83dfc8878a39f643a87fd1912209
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-07-29 21:02:17 +0600

    Finish all task and add placeholder

--- js/style.js
+++ js/style.js
@@ -15,7 +15,7 @@ function randomNumber(min, max) {
 
 // Add event listener to generate button and creat random value 
 document.getElementById("generate-pin").addEventListener("click", function () {
-  var randomValue = Math.round(randomNumber(1000, 9999));
+  var randomValue = Math.round(randomNumber(1000, 9999)); //set range of random number and call function
   document.getElementById("disply-pin").value = randomValue;
   document.getElementById("notify-match").style.display = "none";
   document.getElementById("notify-dont-match").style.display = "none";
@@ -25,27 +25,27 @@ document.getElementById("generate-pin").addEventListener("click", function () {
 const len = document.querySelectorAll(".buton-control").length;
 for (var i = 0; i < len; i++)
   document.querySelectorAll(".buton-control")[i].addEventListener("click", function () {
-    const number = this.innerHTML;
+    const number = this.innerHTML; // get value of button
     const previousValue = document.getElementById("input-pin").value;
     document.getElementById("input-pin").value = previousValue + number;
   });
 
-//set clear button
+//set clear button functionality
 document.getElementById("clear").addEventListener("click", function () {
-  const innerValue = document.getElementById("input-pin").value;
+  var innerValue = document.getElementById("input-pin").value;
   document.getElementById("input-pin").value = '';
 });
 
-// Pin Matching
+// Pin Matching part
 document.getElementById("submit").addEventListener("click", function () {
-  var submitChance = parseInt(document.getElementById("submit-chance").innerHTML); // chance of input password
+  var submitChance = parseInt(document.getElementById("submit-chance").innerHTML); // chance of entering password
   const randNumber = document.getElementById("disply-pin").value;
   const inputedNumber = document.getElementById("input-pin").value;
 
-  if (randNumber.length == 0 || inputedNumber.length == 0) {
+  if (randNumber.length == 0 || inputedNumber.length == 0) { // if empty any input field then show warning
     alert("Doesn't Empty any Field...");
   }
-  else if (randNumber == inputedNumber) {
+  else if (randNumber == inputedNumber) { //if pin matched active this feature
     document.getElementById("notify-dont-match").style.display = "none";
     document.getElementById("notify-match").style.display = "block";
     document.getElementById("submit-chance").innerHTML = 3;
@@ -54,9 +54,10 @@ document.getElementById("submit").addEventListener("click", function () {
   }
   else {
     document.getElementById("notify-dont-match").style.display = "block";
-    submitChance--; // reduce 
+    submitChance--; // reduce try value
     document.getElementById("submit-chance").innerHTML = submitChance;
-    if (submitChance == 0) { // disabled all feature
+
+    if (submitChance == 0) { // disabled all feature after there times fail
       var submitButton = document.getElementById("submit");
       var pinGenaretorButton = document.getElementById("generate-pin");
       pinGenaretorButton.disabled = true;
@@ -69,11 +70,12 @@ document.getElementById("submit").addEventListener("click", function () {
       document.getElementById("disply-pin").value = '';
       document.getElementById("notify-dont-match").style.display = "none";
     }
+
   }
   document.getElementById("input-pin").value = ''; //clear input field
 });
 
-// delete one number per click
+// (backspace button " < ") delete one number per click
 document.getElementById("backspace").addEventListener("click", function () {
   var presentValue = document.getElementById("input-pin").value;
   if (presentValue != '') {
